6330ea0ec2f771cfd698c02c3482ba61af380fa5
Maynard Hartman <mjhartman@brighthouse.com>
2020-01-08 19:27:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
-
+class CashRegister
+  attr_accessor :price, :quantity, :discount, :total
+  
+  last_entry = []
+  
+  def initialize()
+    @total = 0.00
+    @discount = discount
+    @price_each = price
+    @quantity = quantity
+  end
+  
+  def self.total
+    
+  end
+  
+  
+    
+  
